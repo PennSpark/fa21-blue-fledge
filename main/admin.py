024a21594e655de74870526193a498908166d885
lohpaul9 <lohpaul9@seas.upenn.edu>
2022-01-14 21:37:30 -0500

    back to normal

--- main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-#from .models import Tweet
-from .models import Confusion
-from .models import Profile
-
-# Register your models here.
-#admin.site.register(Tweet)
-admin.site.register(Confusion)
-admin.site.register(Profile)
